f27a5a216d5e43f624d3bb8d6a5abc289dd65bfb
frankcrypto2023 <143470927+frankcrypto2023@users.noreply.github.com>
2024-06-08 11:15:47 +0800

    Another implementation of web3.eth.sendSignedTransaction

--- src/tools/testNodeTransactions.js
+++ src/tools/testNodeTransactions.js
@@ -2,6 +2,7 @@ const Web3 = require('web3').Web3;
 const fs = require('fs');
 const path = require('path');
 require('dotenv').config();
+const axios = require("axios");
 
 const account = process.env.ACCOUNT_ADDRESS;
 const privateKey = process.env.PRIVATE_KEY;
@@ -17,7 +18,28 @@ const startTime = new Date();
 const fileNameDatePart = startTime.toISOString().replace(/[:.]/g, '-');
 const rpcUrlNamePart = rpcUrl.replace(/[:/.]/g, '_').slice(0, 30); // 提取RPC URL的部分作为文件名的一部分
 const fileName = `transactions_${fileNameDatePart}_${rpcUrlNamePart}.csv`;
+async function sleep(ms) {
+    return new Promise((resolve) => {
+        setTimeout(resolve, ms);
+    });
+}
+async function waitForHash(txhash) {
+    while (true){
+        try {
+            let res = await web3.eth.getTransactionReceipt(txhash)
+            if (res.status !== undefined) {
+                console.log(`block packed succ ${txhash}`)
+                return res.blockNumber
+            }
 
+        } catch (e) {
+        }
+        await sleep(100);
+    }
+}
+
+const duration = 5 * 60 * 1000;
+const endTime = Date.now() + duration;
 async function sendTransaction(nonce, gasPrice) {
     const tx = {
         from: account,
@@ -28,27 +50,59 @@ async function sendTransaction(nonce, gasPrice) {
         nonce: nonce
     };
 
-    const sentTime = new Date();
-    console.log(`Sending transaction with nonce ${nonce} at ${sentTime.toISOString()}`);
+
 
     try {
         const signedTx = await web3.eth.accounts.signTransaction(tx, privateKey);
-        const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
-        const completedTime = new Date();
-        const costTime = completedTime - sentTime; // 计算完成交易的时间和发送交易时间之间的差值
-        console.log(`Transaction completed with nonce ${nonce} at ${completedTime.toISOString()}`);
 
-        const data = `${sentTime.toISOString()},${completedTime.toISOString()},${costTime},${nonce},${sendAmount},${receipt.transactionHash},${receipt.blockNumber},${rpcUrl}\n`;
+        const sentTime = new Date();
+        console.log(`Sending transaction with nonce ${nonce} at ${sentTime.toISOString()}`);
+        // await waitForHash(signedTx.rawTransaction);
+        const payload = {
+            jsonrpc: "2.0",
+            id: 0,
+            method: "eth_sendRawTransaction",
+            params: [`${signedTx.rawTransaction}`]
+        };
+        const response = await axios.post(rpcUrl, payload)
+        let txHash = response.data.result;
+
+        let completedTime = new Date();
+        let costTime = completedTime - sentTime; // 计算完成交易的时间和发送交易时间之间的差值
+        console.log(`Transaction Send To Node cost:${costTime}ms`);
+
+        console.log("txid",txHash)
+        let blockNumber = await waitForHash(txHash)
+        // const receipt = await web3.eth.sendSignedTransaction(signedTx.rawTransaction);
+        let completedTime1 = new Date();
+        costTime = completedTime1 - sentTime; // 计算完成交易的时间和发送交易时间之间的差值
+        const costTime1 = completedTime1 - completedTime;
+        console.log(`Transaction completed with nonce ${nonce} cost: ${costTime}ms,block packed cost: ${costTime1}ms`);
+        // setTimeout(run, 3000)
+        const data = `${sentTime.toISOString()},${completedTime.toISOString()},${costTime},${costTime1},${nonce},${sendAmount},${txHash},${blockNumber},${rpcUrl}\n`;
         fs.appendFileSync(path.join(__dirname, fileName), data);
     } catch (error) {
         console.error(`Error with transaction for nonce ${nonce}: ${error}`);
     }
+    if (Date.now() >= endTime) {
+        return
+    }
+    setTimeout(run, 100)
+}
+
+
+async function run() {
+    const gasPrice = await web3.eth.getGasPrice();
+    let nonce = await web3.eth.getTransactionCount(account);
+    console.log('nonce:', nonce)
+
+    await sendTransaction(nonce++, gasPrice)
 }
 
 async function startSendingTransactions(interval, duration) {
     const gasPrice = await web3.eth.getGasPrice();
     let nonce = await web3.eth.getTransactionCount(account);
-    console.log('nonce:',nonce)
+    console.log('nonce:', nonce)
 
     const endTime = Date.now() + duration;
     const transactionPromises = [];
@@ -67,6 +121,7 @@ async function startSendingTransactions(interval, duration) {
 }
 
 // 调用函数，每20秒发送一次交易，持续5分钟
-const interval = 20000;
-const duration = 5 * 60 * 1000;
-startSendingTransactions(interval, duration);
+// const interval = 20000;
+// startSendingTransactions(interval, duration);
+
+setTimeout(run, 100)
